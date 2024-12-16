bcac4401dc630959aa5800cebb9a90fdd2319d6d
sik1j <karkisikij2@gmail.com>
2024-12-15 19:02:41 -0800

    impl conditional tokenizing

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -1,15 +1,28 @@
-use crate::tokenizer::TokenType::{LeftParen, Minus, Plus, RightParen, Star};
+use crate::tokenizer::TokenType::{Bang, BangEqual, Equal, EqualEqual, Greater, GreaterEqual, LeftParen, Less, LessEqual, Minus, Plus, RightParen, Star};
 
+#[derive(Debug)]
 enum TokenType {
+    // Single Char
     LeftParen,
     RightParen,
     Plus,
     Minus,
-    Slash,
     Star,
+    // One or Two Chars
+    Equal,
+    EqualEqual,
+    Bang,
+    BangEqual,
+    Less,
+    LessEqual,
+    Greater,
+    GreaterEqual,
+    // Division Or Comment
+    Slash,
 }
 
-struct Token {
+#[derive(Debug)]
+pub struct Token {
     token_type: TokenType,
     line: u64,
     lexeme: String,
@@ -25,31 +38,49 @@ impl Token {
     }
 }
 
-struct Tokenizer {
+pub struct Tokenizer {
     line: u64,
 }
 
 impl Tokenizer {
-    fn tokenize(tokens : String) -> Vec<Token> {
-        Tokenizer::inner(tokens, Tokenizer { line: 0})
+    pub fn tokenize(source: String) -> Vec<Token> {
+        Tokenizer {
+            line: 0,
+        }.tokenize_inner(source)
     }
 
-    fn inner(tokens: String, state: Tokenizer) -> Vec<Token> {
-        let mut tokens = vec![];
+    fn tokenize_inner(&mut self, source: String) -> Vec<Token> {
+        let mut chars = source.chars().peekable();
 
-        match tokens.chars().next() {
-            None => { tokens }
-            Some(c) =>
-                {
-                    let tok = |t| {Token::new(t, state.line, c)};
-                    match c {
-                        '(' => tok(LeftParen),
-                        ')' => tok(RightParen),
-                        '+' => tok(Plus),
-                        '-' => tok(Minus),
-                        '*' => tok(Star)
-                    }
+        let mut tokens = vec![];
+        while let Some(c) = chars.next() {
+            let tok = |token_type: TokenType| { Token { token_type, lexeme: c.to_string(), line: self.line } };
+            let mut equal_type_tok = |one, two| {
+                if let Some('=') = chars.next_if_eq(&'=') {
+                    Token { token_type: two, lexeme: c.to_string() + "=", line: self.line }
+                } else {
+                    Token { token_type: one, lexeme: c.to_string(), line: self.line }
                 }
-        }
+            };
+
+            tokens.push(match c {
+                // one char
+                '(' => tok(LeftParen),
+                ')' => tok(RightParen),
+                '+' => tok(Plus),
+                '-' => tok(Minus),
+                '*' => tok(Star),
+                // one or two char
+                '=' => equal_type_tok(Equal, EqualEqual),
+                '!' => equal_type_tok(Bang, BangEqual),
+                '>' => equal_type_tok(Greater, GreaterEqual),
+                '<' => equal_type_tok(Less, LessEqual),
+                // whitespace
+                ' ' => continue,
+                _ => panic!(),
+            });
+        };
+
+        tokens
     }
 }
